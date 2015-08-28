6b0d04c5e3e58a92cffa63047b64cb7027308414
roshanbasnet <roshanbasnet26@gmail.com>
2015-08-28 07:51:53 +0545

    setting folder structure

---
+++ PresentationPopcorn/src/main/java/com/rara/Application.java
@@ -0,0 +1,15 @@
+package com.rara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sparkle on 8/28/15.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
